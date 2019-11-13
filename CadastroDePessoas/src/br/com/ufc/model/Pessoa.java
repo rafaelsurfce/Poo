2f2a58544db12e932feff32bc05038122a9c08d7
Rafael Lima <rafael_surfce@hotmail.com>
2019-11-13 00:45:06 -0300

    Add files via upload

---
+++ CadastroDePessoas/src/br/com/ufc/model/Pessoa.java
@@ -0,0 +1,41 @@
+package br.com.ufc.model;
+
+public class Pessoa {
+	private String nome;
+	private String cpf;
+	private int idade;
+	
+	
+	
+	public String getNome() {
+		return this.nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCpf() {
+		return this.cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public int getIdade() {
+		return this.idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	
+	
+	public String toString() {
+		return  this.nome+"                                                            "+this.cpf+"                                                                       "+this.idade;
+		 
+		
+	}
+
+	
+	
+	
+	
+
+}
